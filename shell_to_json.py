1ae5c613147e071086f89d3fe470ef16bc234d80
Richard Neave <neaverds@icloud.com>
2019-08-07 17:10:46 +0100

    init

---
+++ shell_to_json.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys, getopt, re, json
+
+def main(argv, stdin):
+    try:
+        opts, args = getopt.getopt(argv,"h:p:",["headers=","pretty="])
+    except getopt.GetoptError:
+        print 'shell_to_json.py -h "<header>,<header>" -p true'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--headers"):
+            headers = [x.strip() for x in arg.split(',')]
+        elif opt in ("-p", "--pretty"):
+            pretty = arg
+
+    pattern = re.compile("\s+")
+    list = []
+    for line in stdin:
+        line = line.strip()
+        row = pattern.split(line)
+
+        if len(row) == len(headers):
+            obj = {} 
+            for i, header in enumerate(headers):
+                obj[header] = row[i]
+            list.append(obj)
+        
+    if pretty == 'true':
+        print json.dumps(list, indent=4)
+    else:
+        print json.dumps(list)
+if __name__ == "__main__":
+   main(sys.argv[1:], sys.stdin)
+
---
+++ test.sh
@@ -0,0 +1,2 @@
+ls -la / | python shell_to_json.py -h "permissions,count,owner,group,size,day,month,time,name" -p true
+
