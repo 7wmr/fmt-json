ac9c07079165c69dcbe5bf344f2b911cffdec739
Richard Neave <neaverds@icloud.com>
2019-08-08 12:37:24 +0100

    update tests

--- test.sh
+++ test.sh
@@ -1,20 +1,20 @@
 
 # List directories.
-echo '\n\n###Directories:\n\n```json'
+echo '\n\n### Directories:\n\n```json'
 ls -la ../ | python fmt-json.py -h "permissions,count,owner,group,size,day,month,time_year,name" -P
 echo '```'
 
 # List disk mounts.
-echo '\n\n###Disk Mounts:\n\n```json'
+echo '\n\n### Disk Mounts:\n\n```json'
 df -Ph | python fmt-json.py -h "filesystem,blocks,used,available,capacity,mounted_on" -d "\s{2,}" -s 1
 echo '```'
 
 # List processes.
-echo '\n\n###Processes:\n\n```json'
+echo '\n\n### Processes:\n\n```json'
 ps alx | head -n 10 | python fmt-json.py -h "user,pid,ppid,cpu,pri,ni,vsz,rss,wchan,stat,tt,time,command" -s 1 -P 
 echo '```'
 
 # Active connections.
-echo '\n\n###Connections:\n\n```json'
+echo '\n\n### Connections:\n\n```json'
 netstat -a | head -n 10 | python fmt-json.py -h "protocol,receive_queue,send_queue,local_address,foreign_address,state" -s 1 -P
 echo '```'
--- test_centos.sh
+++
@@ -1,2 +0,0 @@
- docker run -v $(pwd):/test/ -it centos sh -c 'cd /test && sh run_test.sh' > README.md
-
