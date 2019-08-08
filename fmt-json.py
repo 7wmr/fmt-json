6f3817005537db86204f95c3ca06e70097f07a63
Richard Neave <neaverds@icloud.com>
2019-08-08 10:14:50 +0100

    add handling for additional commands and delimiter options

--- fmt-json.py
+++ fmt-json.py
@@ -3,23 +3,33 @@
 import sys, getopt, re, json
 
 def main(argv, stdin):
+    skip = 0
+    pretty = "false"
+    delimiter = "\s+"
     try:
-        opts, args = getopt.getopt(argv,"h:p:",["headers=","pretty="])
+        opts, args = getopt.getopt(argv,"h:p:s:d:",["headers=","pretty=", "skip-lines=", "delimiter="])
     except getopt.GetoptError:
-        print 'shell_to_json.py -h "<header>,<header>" -p true'
+        sys.stdout.write('shell_to_json.py -h "<header>,<header>" -p true\n')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-h", "--headers"):
             headers = [x.strip() for x in arg.split(',')]
         elif opt in ("-p", "--pretty"):
             pretty = arg
+        elif opt in ("-s", "--skip-lines"):
+            skip = int(arg)
+        elif opt in ("-d", "--delimiter"):
+            delimiter = arg
 
-    pattern = re.compile("\s+")
+    pattern = re.compile(delimiter)
     list = []
-    for line in stdin:
+    for l, line in enumerate(stdin):
+        if int(l + 1) <= skip: 
+            continue
+       
         line = line.strip()
         row = pattern.split(line)
-
+        
         if len(row) == len(headers):
             obj = {} 
             for i, header in enumerate(headers):
@@ -27,9 +37,9 @@ def main(argv, stdin):
             list.append(obj)
         
     if pretty == 'true':
-        print json.dumps(list, indent=4)
+        sys.stdout.write(json.dumps(list, indent=4) + '\n')
     else:
-        print json.dumps(list)
+        sys.stdout.write(json.dumps(list) + '\n')
 
 if __name__ == "__main__":
    main(sys.argv[1:], sys.stdin)
--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
-ls -la / | python shell_to_json.py -h "permissions,count,owner,group,size,day,month,time,name" -p true
 
+# List directories.
+ls -la / | python fmt-json.py -h "permissions,count,owner,group,size,day,month,time,name" -p true
+
+# List disk mounts.
+df -Ph | python fmt-json.py -h "filesystem,blocks,used,available,capacity,mounted_on" -p true -d "\s{2,}" -s 1
+
+# List processes.
+ps alx | head -n 10 | python fmt-json.py -h "user,pid,ppid,cpu,pri,ni,vsz,rss,wchan,stat,tt,time,command" -s 1 -p true
