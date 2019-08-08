1781b3c3e7856d9c258e5dbb047be3b36bb73763
Richard Neave <neaverds@icloud.com>
2019-08-08 10:31:42 +0100

    update pretty print to switch

--- fmt-json.py
+++ fmt-json.py
@@ -4,18 +4,18 @@ import sys, getopt, re, json
 
 def main(argv, stdin):
     skip = 0
-    pretty = "false"
+    pretty = False
     delimiter = "\s+"
     try:
-        opts, args = getopt.getopt(argv,"h:p:s:d:",["headers=","pretty=", "skip-lines=", "delimiter="])
+        opts, args = getopt.getopt(argv,"h:Ps:d:",["headers=","pretty", "skip-lines=", "delimiter="])
     except getopt.GetoptError:
-        sys.stdout.write('shell_to_json.py -h "<header>,<header>" -p true\n')
+        sys.stdout.write('shell_to_json.py -h "<header>,<header>" -P\n')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-h", "--headers"):
             headers = [x.strip() for x in arg.split(',')]
-        elif opt in ("-p", "--pretty"):
-            pretty = arg
+        elif opt in ("-P", "--pretty"):
+            pretty = True
         elif opt in ("-s", "--skip-lines"):
             skip = int(arg)
         elif opt in ("-d", "--delimiter"):
@@ -36,7 +36,7 @@ def main(argv, stdin):
                 obj[header.lower()] = row[i]
             list.append(obj)
         
-    if pretty == 'true':
+    if pretty:
         sys.stdout.write(json.dumps(list, indent=4) + '\n')
     else:
         sys.stdout.write(json.dumps(list) + '\n')
--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 
 # List directories.
-ls -la / | python fmt-json.py -h "permissions,count,owner,group,size,day,month,time,name" -p true
+echo '\n\nDirectories:\n\n'
+ls -la ../ | python fmt-json.py -h "permissions,count,owner,group,size,day,month,time,name" -P
 
 # List disk mounts.
-df -Ph | python fmt-json.py -h "filesystem,blocks,used,available,capacity,mounted_on" -p true -d "\s{2,}" -s 1
+echo '\n\nDisk Mounts:\n\n'
+df -Ph | python fmt-json.py -h "filesystem,blocks,used,available,capacity,mounted_on" -d "\s{2,}" -s 1
 
 # List processes.
-ps alx | head -n 10 | python fmt-json.py -h "user,pid,ppid,cpu,pri,ni,vsz,rss,wchan,stat,tt,time,command" -s 1 -p true
+echo '\n\nProcesses:\n\n'
+ps alx | head -n 10 | python fmt-json.py -h "user,pid,ppid,cpu,pri,ni,vsz,rss,wchan,stat,tt,time,command" -s 1 -P 
