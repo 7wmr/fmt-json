9b64a4509db246dcd84d2dcbb3a2378d3f8c23da
Richard Neave <neaverds@icloud.com>
2019-08-08 12:22:06 +0100

    add ignore file

--- test.sh
+++ test.sh
@@ -1,12 +1,20 @@
 
 # List directories.
-echo '\n\nDirectories:\n\n'
-ls -la ../ | python fmt-json.py -h "permissions,count,owner,group,size,day,month,time,name" -P
+echo '\n\n###Directories:\n\n```json'
+ls -la ../ | python fmt-json.py -h "permissions,count,owner,group,size,day,month,time_year,name" -P
+echo '```'
 
 # List disk mounts.
-echo '\n\nDisk Mounts:\n\n'
+echo '\n\n###Disk Mounts:\n\n```json'
 df -Ph | python fmt-json.py -h "filesystem,blocks,used,available,capacity,mounted_on" -d "\s{2,}" -s 1
+echo '```'
 
 # List processes.
-echo '\n\nProcesses:\n\n'
+echo '\n\n###Processes:\n\n```json'
 ps alx | head -n 10 | python fmt-json.py -h "user,pid,ppid,cpu,pri,ni,vsz,rss,wchan,stat,tt,time,command" -s 1 -P 
+echo '```'
+
+# Active connections.
+echo '\n\n###Connections:\n\n```json'
+netstat -a | head -n 10 | python fmt-json.py -h "protocol,receive_queue,send_queue,local_address,foreign_address,state" -s 1 -P
+echo '```'
